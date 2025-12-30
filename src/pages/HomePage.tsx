679dfc37a7550e657a80b9a4c83a41c8dc16ae58
tatanlejo <tatanlejo17@gmail.com>
2025-12-30 15:32:47 -0500

    Crear un spin mientras se realiza la petición a la Poke Api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,6 +12,7 @@ export default function DashboardPage() {
     const [search, setSearch] = useState('');
     const [page, setPage] = useState(0);
     const ITEMS_FOR_PAGE = 20;
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchPokemons();
@@ -19,12 +20,16 @@ export default function DashboardPage() {
 
     const fetchPokemons = async () => {
         try {
+            setLoading(true);
+
             const offset = page * ITEMS_FOR_PAGE;
             const data = await PokeClient<PokeApiResponse>(`/pokemon?limit=${ITEMS_FOR_PAGE}&offset=${offset}`);
 
             setPokemons(data.results);
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -37,54 +42,60 @@ export default function DashboardPage() {
 
     return (
         <div className="min-h-screen bg-gray-50 p-8">
-            <div className='max-w-6xl mx-auto px-4'>
-                <div className='flex justify-between'>
-                    <span className='text-2xl font-bold text-lime-500 text-shadow-sm'>Listado de pokemones</span>
-                    <input
-                        type="text"
-                        placeholder="Buscar Pokemón..."
-                        className="w-80 p-2 mb-6 rounded-lg shadow-sm border"
-                        onChange={(e) => setSearch(e.target.value)}
-                    />
+            {isLoading ? (
+                <div className="flex justify-center items-center h-80">
+                    <div className="animate-spin rounded-full h-30 w-30 border-b-6 border-violet-600 transition-all"></div>
                 </div>
+            ) : (
+                <div className='max-w-6xl mx-auto px-4'>
+                    <div className='flex justify-between'>
+                        <span className='text-2xl font-bold text-lime-500 text-shadow-sm'>Listado de pokemones</span>
+                        <input
+                            type="text"
+                            placeholder="Buscar Pokemón.. ."
+                            className="w-80 p-2 mb-6 rounded-lg shadow-sm border"
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="grid grid-flow-col grid-rows-4 grid-cols-5 gap-4">
+                        {filtrarPokemons.map((pokemon) => {
+                            const id = pokemon.url.split('/')[6];
+
+                            return (
+                                <Link to={`/pokemon/${id}`} key={pokemon.name} className="block group">
+                                    <div className="bg-white p-4 rounded-xl shadow hover:shadow-lg transition transform hover:-translate-y-1 text-center border border-gray-100">
+                                        <img
+                                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                                            alt={pokemon.name}
+                                            className="mx-auto w-24 h-24 group-hover:scale-110 transition"
+                                        />
+                                        <p className="capitalize font-semibold text-gray-700 mt-2">{pokemon.name}</p>
+                                    </div>
+                                </Link>
+                            );
+                        })}
+                    </div>
 
 
-                <div className="grid grid-flow-col grid-rows-4 grid-cols-5 gap-4">
-                    {filtrarPokemons.map((pokemon) => {
-                        const id = pokemon.url.split('/')[6];
-
-                        return (
-                            <Link to={`/pokemon/${id}`} key={pokemon.name} className="block group">
-                                <div className="bg-white p-4 rounded-xl shadow hover:shadow-lg transition transform hover:-translate-y-1 text-center border border-gray-100">
-                                    <img
-                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-                                        alt={pokemon.name}
-                                        className="mx-auto w-24 h-24 group-hover:scale-110 transition"
-                                    />
-                                    <p className="capitalize font-semibold text-gray-700 mt-2">{pokemon.name}</p>
-                                </div>
-                            </Link>
-                        );
-                    })}
+                    <div className="flex justify-center mt-8 gap-4">
+                        <button
+                            disabled={page === 0}
+                            onClick={() => setPage(p => p - 1)}
+                            className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50 hover:bg-gray-300"
+                        >
+                            Anterior
+                        </button>
+                        <span className="px-4 py-2 font-bold">Página {page + 1}</span>
+                        <button
+                            onClick={() => setPage(p => p + 1)}
+                            className="px-4 py-2 bg-violet-600 text-white rounded hover:bg-violet-500"
+                        >
+                            Siguiente
+                        </button>
+                    </div>
                 </div>
-
-                <div className="flex justify-center mt-8 gap-4">
-                    <button
-                        disabled={page === 0}
-                        onClick={() => setPage(p => p - 1)}
-                        className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50 hover:bg-gray-300"
-                    >
-                        Anterior
-                    </button>
-                    <span className="px-4 py-2 font-bold">Página {page + 1}</span>
-                    <button
-                        onClick={() => setPage(p => p + 1)}
-                        className="px-4 py-2 bg-violet-600 text-white rounded hover:bg-violet-500"
-                    >
-                        Siguiente
-                    </button>
-                </div>
-            </div>
+            )}
         </div>
     );
 }
